a62c6a91f92dfe3d02d8aacef3fb4d32c4d1b21d
Simon Elvery <simon@elvery.net>
2020-05-22 15:13:47 +1000

    Use component library version of EmbedContainer

--- src/components/Embed/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./styles.scss";
-
-export default ({ children, embed }) => {
-  const cns = [styles.embed];
-  if (embed === "right") cns.push(styles.right);
-  if (embed === "full") cns.push(styles.full);
-  return <aside className={cns.join(" ")}>{children}</aside>;
-};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-// import "regenerator-runtime/runtime.js";
-// import "core-js/features/symbol";
-// import "core-js/features/symbol/iterator";
-
 import React from "react";
 import { render } from "react-dom";
 import * as a2o from "@abcnews/alternating-case-to-object";
 import { App } from "./components/App";
-import Embed from "./components/Embed";
+import { EmbedContainer } from "@abcnews/story-lab-component-library";
 
 const domready = fn => {
   /in/.test(document.readyState) ? setTimeout(() => domready(fn), 9) : fn();
@@ -29,9 +25,9 @@ const renderEmbed = () =>
 
       render(
         props.embed ? (
-          <Embed embed={props.embed}>
+          <EmbedContainer embed={props.embed}>
             <App {...props} />
-          </Embed>
+          </EmbedContainer>
         ) : (
           <App {...props} />
         ),
