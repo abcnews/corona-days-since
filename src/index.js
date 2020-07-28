f7377a58374063cf0281f14ff237e5601be50810
Simon Elvery <simon@elvery.net>
2020-07-28 13:02:25 +1000

    Use @abcnews/mount-utils

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import { render } from "react-dom";
 import * as a2o from "@abcnews/alternating-case-to-object";
 import { App } from "./components/App";
 import { EmbedContainer } from "@abcnews/story-lab-component-library";
+import { prefixedMountSelector } from '@abcnews/mount-utils'
 
 const domready = fn => {
   /in/.test(document.readyState) ? setTimeout(() => domready(fn), 9) : fn();
 };
 
 const renderEmbed = () =>
-  [...document.querySelectorAll(`a[id^=dayssince],a[name^=dayssince]`)].map(
+  [...document.querySelectorAll(prefixedMountSelector('dayssince'))].map(
     anchorEl => {
       const props = a2o(
         anchorEl.getAttribute("id") || anchorEl.getAttribute("name")
