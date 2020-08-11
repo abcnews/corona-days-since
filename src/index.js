384525f93b17fac6c6e54279309f32ad53cdc708
Simon Elvery <simon@elvery.net>
2020-08-11 14:12:12 +1000

    Update depndencies

--- src/index.js
+++ src/index.js
@@ -3,27 +3,16 @@ import { render } from "react-dom";
 import * as a2o from "@abcnews/alternating-case-to-object";
 import { App } from "./components/App";
 import { EmbedContainer } from "@abcnews/story-lab-component-library";
-import { prefixedMountSelector } from '@abcnews/mount-utils'
+import { selectMounts, getMountValue } from '@abcnews/mount-utils'
 
 const domready = fn => {
   /in/.test(document.readyState) ? setTimeout(() => domready(fn), 9) : fn();
 };
 
 const renderEmbed = () =>
-  [...document.querySelectorAll(prefixedMountSelector('dayssince'))].map(
-    anchorEl => {
-      const props = a2o(
-        anchorEl.getAttribute("id") || anchorEl.getAttribute("name")
-      );
-      const mountEl = document.createElement("div");
-
-      Object.keys(props).forEach(
-        propName => (mountEl.dataset[propName] = props[propName])
-      );
-
-      anchorEl.parentElement.insertBefore(mountEl, anchorEl);
-      anchorEl.parentElement.removeChild(anchorEl);
-
+  selectMounts('dayssince')
+    .map(mountEl => {
+      const props = a2o(getMountValue(mountEl));
       render(
         props.embed ? (
           <EmbedContainer embed={props.embed}>
