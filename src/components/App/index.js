de61405449cce33eb6ac4e79f3331981456244a4
Simon Elvery <simon@elvery.net>
2022-01-11 16:52:34 +1000

    Don't render this anymore

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,40 +4,41 @@ import worm from "./worm.svg";
 import { csvParse } from "d3-dsv";
 import { parse, format } from "date-fns";
 
+// If this is ever revived the new data source is probably https://www.abc.net.au/dat/news/interactives/covid19-data/federal-government-data.csv
 const dataUrl =
   "https://covid-sheets-mirror.web.app/api?sheet=1nUUU5zPRPlhAXM_-8R7lsMnAkK4jaebvIL5agAaKoXk&range=Daily%20Count%20States!A:E";
 const metadataUrl =
   "https://covid-sheets-mirror.web.app/api?sheet=1nUUU5zPRPlhAXM_-8R7lsMnAkK4jaebvIL5agAaKoXk&range=Metadata";
 
-const promiseChainSpy = d => {
+const promiseChainSpy = (d) => {
   console.log(d);
   return d;
 };
 
-const descending = (accessor = d => d) => (a, b) =>
+const descending = (accessor = (d) => d) => (a, b) =>
   accessor(a) > accessor(b) ? -1 : accessor(b) > accessor(a) ? 1 : 0;
 
-const ascending = (accessor = d => d) => (a, b) =>
+const ascending = (accessor = (d) => d) => (a, b) =>
   accessor(a) < accessor(b) ? -1 : accessor(b) < accessor(a) ? 1 : 0;
 
-export const App = props => {
+export const App = (props) => {
   const [data, setData] = useState(null);
   const [lastUpdate, setLastUpdate] = useState(null);
 
   useEffect(() => {
     fetch(dataUrl)
-      .then(res => res.text())
+      .then((res) => res.text())
       .then(csvParse)
-      .then(raw =>
+      .then((raw) =>
         raw
-          .map(d => ({
+          .map((d) => ({
             date: parse(d["Date announced"], "dd/MM/yyyy", new Date()),
             added: d["New cases"] === "" ? null : +d["New cases"],
-            jurisdiction: d["State/territory"]
+            jurisdiction: d["State/territory"],
           }))
-          .sort(descending(d => +d.date))
+          .sort(descending((d) => +d.date))
       )
-      .then(raw =>
+      .then((raw) =>
         raw.reduce((m, { jurisdiction, added, date }) => {
           const count = m.get(jurisdiction);
 
@@ -55,21 +56,21 @@ export const App = props => {
           return m;
         }, new Map())
       )
-      .then(map => {
+      .then((map) => {
         const daysSince = [];
         map.forEach((count, jurisdiction) =>
           daysSince.push({ days: count.val, jurisdiction })
         );
-        return daysSince.sort(ascending(d => d.days));
+        return daysSince.sort(ascending((d) => d.days));
       })
       .then(setData);
   }, []);
 
   useEffect(() => {
     fetch(metadataUrl)
-      .then(res => res.text())
+      .then((res) => res.text())
       .then(csvParse)
-      .then(data => {
+      .then((data) => {
         return setLastUpdate(
           parse(data[1].Value, "dd/MM/yyyy HH:mm:ss", new Date())
         );
@@ -86,7 +87,7 @@ export const App = props => {
           display: "flex",
           justifyContent: "center",
           flexWrap: "wrap",
-          textAlign: "center"
+          textAlign: "center",
         }}
       >
         {data.map(({ days, jurisdiction }) => (
